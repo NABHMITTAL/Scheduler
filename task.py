70d9876674a8c164d0a17e420fffb7cc225168f9
NABHMITTAL <nabh.mittalss@gmail.com>
2026-05-30 18:25:07 +0530

    Implemented Task serialization, JSON save/load, and task display v0.1.2

--- task.py
+++ task.py
@@ -12,33 +12,69 @@ class Task():
     def display(self):
         print(f"{self.id}. {self.title} | Priority: {self.priority} | Status: {self.done}")
 
+    def toDict(self):
+        return{
+            "id": self.id,
+             "title": self.title,
+            "priority": self.priority,
+            "complete": self.done
+        }
+    
+    @classmethod
+    def fromDict(cls,data):
+        task = cls(
+            data["id"],
+            data["title"],
+            data["priority"],
+        )
+
+        task.done = data["complete"]
+        return task
+
+def  loadTask():
+    loadArray = []
+    try:
+        with open("taskList.json","r") as file:
+            loadedData = json.load(file)
+        for data in loadedData:
+            loadArray.append(Task.fromDict(data))
+        return loadArray
+    except:
+        return []
+    
 try:
-    with open("taskList.json","r") as file:
-        tasks = json.load(file)
+    taskArray = loadTask()
 except:
-    task = []
+    taskArray = []
 
 def  enterTask():
+    print("input Done in Title to finsh givng the input")
     while True:
-        taskIn = input("Enter a Task and Enter Done to finish inputting")
-        if taskIn == "Done":
+        taskId = input("Enter a Task id")
+        title = input("Enter a Task Title")
+        priority = int(input("Enter a Task Priority"))
+        if title == "Done":
             break
         else:
-          tasks.append(taskIn)
+            newTask = Task(taskId,title,priority) 
+            taskArray.append(newTask)
 
 def deleteTask():
     try:
         item = input("Enter Serial Number in range to delete")
-        del tasks[int(item)-1]
+        del taskArray[int(item)-1]
     except:
         print("Invalid Input bruh")
 
-def  showTask():
-    for i in range(len(tasks)):
-        print(f"{i+1}. {tasks[i]}")
-
 def saveChanges():
     #ask for permission before replacing
-    with open("taskList.json","w") as file:
-        json.dump(tasks,file)
+    saveData = []
+    for task in taskArray:
+        saveData.append(task.toDict())
 
+    with open("taskList.json","w") as file:
+           json.dump(saveData,file)
+
+def displayTasks():
+    for task in taskArray:
+        task.display()
