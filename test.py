f630cab0f21b36d4ee7979edf4e614589812ee70
NABHMITTAL <nabh.mittalss@gmail.com>
2026-06-12 00:08:19 +0530

    Updated to account for the renamed files

--- test.py
+++ test.py
@@ -1,17 +1,23 @@
-from task import Task,Project
+from project import Task,Project
 
 print("=== Testing Project Display ===")
 
 p = Project("Test")
+p1 = Project("Code")
 
-print("\n\n ------Object Formation works------")
+print("\n\n ------Object Formation------")
 p.tasks.append(Task("1", "Homework", 2))
 p.tasks.append(Task("2", "Study", 1))
 p.tasks.append(Task("3","Help",3))
 
+p1.tasks.append(Task("1", "Homework", 2))
+p1.tasks.append(Task("2", "Study", 1))
+p1.tasks.append(Task("3","Help",3))
+
 print("\n\n ------Project Display------")
 
 p.displayProject()
+p1.displayProject()
 
 print("\n\n ------Project to dict------")
 data = p.projectToDict()
